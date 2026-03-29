6387593ec803024e206e0664eab9897f51ea744b
Bibiana <bibianabertuarios@gmail.com>
2026-03-29 19:04:16 +0200

    Add Meta Pixel

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,12 @@ export default function RootLayout({ children }) {
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" strategy="afterInteractive" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" strategy="afterInteractive" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init","965500609494866");fbq("track","PageView");`}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
