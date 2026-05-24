3a4d30c0cb995fc81116a7cf3a88e179333b3473
Bibiana Bertua Rios <bibianabertuarios@MacBook-Pro-de-Bibiana.local>
2026-05-24 19:45:33 +0200

    add Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: 'Lumera - Cuando tu cuerpo cambia, tener un plan lo cambia todo',
   description: 'Acompañamiento personalizado para mujeres 40+',
 }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
           {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init","965500609494866");fbq("track","PageView");`}
         </Script>
         {children}
+        <Analytics />
       </body>
     </html>
   )
